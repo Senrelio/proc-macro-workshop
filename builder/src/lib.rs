bee408653b5f468f6cbd93a28d814e5bd56954f5
Iwazaki <iwazaki@uta-senzen.com>
2020-10-20 21:46:31 +0800

    pass builder test03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,13 +2,96 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed};
+use syn::{parse_macro_input, DeriveInput, Type};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let name = ast.ident;
     let b_name = format_ident!("{}Builder", name);
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named: fields, .. }),
+        ..
+    }) = ast.data
+    {
+        fields
+    } else {
+        unimplemented!("builder proc-macro can only apply to struct.");
+    };
+
+    let b_fields = fields.iter().map(|f| {
+        let f_name = &f.ident;
+        let ty = &f.ty;
+        match is_option(ty).is_some() || is_vec(ty).is_some() {
+            true => {
+                quote! {
+                    #f_name: #ty
+                }
+            }
+            false => {
+                quote! {
+                    #f_name: Option<#ty>
+                }
+            }
+        }
+    });
+
+    let b_init = fields.iter().map(|f| {
+        let f_name = &f.ident;
+        let ty = &f.ty;
+        match is_vec(ty).is_some() {
+            true => quote! {
+                #f_name: Vec::new()
+            },
+            false => quote! {
+                #f_name: None
+            },
+        }
+    });
+
+    let extract = fields.iter().map(|f| {
+        let f_name = &f.ident;
+        let ty = &f.ty;
+        let is_option = is_option(ty).is_some();
+        let is_vec = is_vec(ty).is_some();
+        match (is_option, is_vec) {
+            (true, false) => quote! {
+                #f_name: self.#f_name
+            },
+            (true, true) => unimplemented!(),
+            (false, false) => quote! {
+                #f_name: self.#f_name.unwrap()
+            },
+            (false, true) => quote! {
+                #f_name: self.#f_name
+            },
+        }
+    });
+
+    let b_methods = fields.iter().map(|f| {
+        let f_name = &f.ident;
+        let ty = &f.ty;
+        let is_option = is_option(ty);
+        let is_vec = is_vec(ty);
+        match (is_option, is_vec) {
+            (Some(t), None) => quote! {
+                fn #f_name(&mut self, #f_name: #t) {
+                    self.#f_name = #f_name;
+                }
+            },
+            (Some(_), Some(_)) => unimplemented!(),
+            (None, None) => quote! {
+                fn #f_name(&mut self, #f_name: #ty) {
+                    self.#f_name = Some(#f_name);
+                }
+            },
+            (None, Some(_)) => quote! {
+                fn #f_name(&mut self, #f_name: #ty) {
+                    self.#f_name = #f_name;
+                }
+            },
+        }
+    });
 
     let expand = quote! {
         impl #name {
@@ -41,3 +124,52 @@ pub fn derive(input: TokenStream) -> TokenStream {
     expand.into()
 }
 
+fn is_option(ty: &Type) -> Option<&Type> {
+    if let Type::Path(syn::TypePath { path, .. }) = ty {
+        assert_eq!(path.segments.len(), 1, "cannot handle type with more than 1 segment");
+        let seg = &path.segments[0];
+        if seg.ident != "Option" {
+            return None
+        }
+        let args = &seg.arguments;
+        if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+            args: inside_bracket,..
+        }) = args {
+            assert_eq!(inside_bracket.len(), 1, "cannot have more than one generic type");
+            if let syn::GenericArgument::Type(inner_type) = &inside_bracket[0] {
+                Some(inner_type)
+            } else {
+                unimplemented!()
+            }
+        } else {
+            None
+        }
+    } else {
+        unimplemented!()
+    }
+}
+
+fn is_vec(ty: &Type) -> Option<&Type> {
+    if let Type::Path(syn::TypePath { path, .. }) = ty {
+        assert_eq!(path.segments.len(), 1, "cannot handle type with more than 1 segment");
+        let seg = &path.segments[0];
+        if seg.ident != "Vec" {
+            return None
+        }
+        let args = &seg.arguments;
+        if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+            args: inside_bracket,..
+        }) = args {
+            assert_eq!(inside_bracket.len(), 1, "cannot have more than one generic type");
+            if let syn::GenericArgument::Type(inner_type) = &inside_bracket[0] {
+                Some(inner_type)
+            } else {
+                unimplemented!()
+            }
+        } else {
+            None
+        }
+    } else {
+        unimplemented!()
+    }
+}
