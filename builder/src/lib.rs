e689ec4965edf64e3f35105161f5033374686a9e
Iwazaki <iwazaki@uta-senzen.com>
2020-10-20 11:13:20 +0800

    builder skeleton

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,43 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = ast.ident;
+    let b_name = format_ident!("{}Builder", name);
 
-    unimplemented!()
+    let expand = quote! {
+        impl #name {
+            fn builder() -> #b_name {
+                #b_name {
+                    #(
+                        #b_init
+                    ),*
+                }
+            }
+        }
+        struct #b_name {
+            #(
+                #b_fields
+            ),*
+        }
+        impl #b_name {
+            fn build(self) -> #name {
+                #name {
+                    #(
+                        #extract
+                    ),*
+                }
+            }
+            #(
+                #b_methods
+            )*
+        }
+    };
+    expand.into()
 }
+
